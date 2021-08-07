4b656e76396d4df474aab45bd5e00028a95c54df
Hider Sanchez <hider.sanchez@vasslatam.com>
2021-08-07 06:24:53 +0100

    Index Controller

--- src/main/java/sr/hit/helloworld/HelloWorldApplication.java
+++ src/main/java/sr/hit/helloworld/HelloWorldApplication.java
@@ -2,8 +2,11 @@ package sr.hit.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import sr.hit.helloworld.config.FirebaseProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FirebaseProperties.class)
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
--- src/main/java/sr/hit/helloworld/controller/IndexController.java
+++ src/main/java/sr/hit/helloworld/controller/IndexController.java
@@ -3,29 +3,34 @@ package sr.hit.helloworld.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sr.hit.helloworld.config.FirebaseProperties;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
+    private final FirebaseProperties firebaseProperties;
 
     @GetMapping(value = "/")
     public ResponseEntity<Object> index() throws JsonProcessingException {
         var mapper = new ObjectMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
         var response = new HashMap<String, String>();
         response.put("message", "Hellow World from Docker");
         response.put("time", LocalDateTime.now(ZoneId.of("UTC-5")).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.SSS")));
-
-        return ResponseEntity.accepted().body(mapper.writeValueAsString(response));
-
+        response.put("firebase.user", firebaseProperties.getUser());
+        response.put("firebase.pwd", firebaseProperties.getPwd());
+        var result = mapper.writer().with(SerializationFeature.INDENT_OUTPUT).writeValueAsString(response);
+        log.info(" result with format {}", result);
+        return ResponseEntity.accepted().body(result);
     }
-
-
 }
